2567378a274641aa89d436990c4fef556420af81
xusanboy-codes <110672437+xusanboy-codes@users.noreply.github.com>
2022-08-18 13:28:32 +0500

    Add files via upload

---
+++ composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'aabduvohid/longpolling',
+  ),
+  'versions' => 
+  array (
+    'aabduvohid/longpolling' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'telegram-bot/api' => 
+    array (
+      'pretty_version' => 'v2.3.24',
+      'version' => '2.3.24.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '579f64d0ffe7f21d1f61cb8734e297686b286bf7',
+    ),
+  ),
+);
